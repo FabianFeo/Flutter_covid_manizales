b43b4696249ee081e419ad82f763335426c25578
juyliantamayo <juliantamayo16@outlook.es>
2021-03-29 10:57:37 -0500

    barrios

--- lib/src/service/barrio.service.dart
+++ lib/src/service/barrio.service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 class BarrioService {
   Future<List> getBarrio(String codigoBarrio) async {
     http.Response response = await http.get(
-        ' /geo/barrios?comuna=' +
+        'https://covidalert.com.co/api/geo/barrios?comuna=' +
             codigoBarrio);
     Map<String, dynamic> body = jsonDecode(response.body);
     List<dynamic> data = body['data'];
