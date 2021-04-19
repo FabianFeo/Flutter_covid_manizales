ca354af32b52466ad8604ba3789054a760cff238
EyeWithItHandy <64708166+EyeWithItHandy@users.noreply.github.com>
2021-04-19 13:15:29 -0500

    semaforo

--- lib/src/view/Contagios.dart
+++ lib/src/view/Contagios.dart
@@ -14,6 +14,7 @@ class Contagios extends StatefulWidget {
 }
 
 class _ContagiosState extends State<Contagios> with TickerProviderStateMixin {
+  String nivel = 'Bajo';
   bool _sesion = false;
   TabController _tabController;
   @override
@@ -40,7 +41,7 @@ class _ContagiosState extends State<Contagios> with TickerProviderStateMixin {
                         )))),
         body: SingleChildScrollView(
           child: Container(
-            margin: EdgeInsets.only(top: height / 10),
+            margin: EdgeInsets.only(top: height / 40),
             alignment: Alignment.centerLeft,
             child: Column(
               children: [
@@ -63,6 +64,25 @@ class _ContagiosState extends State<Contagios> with TickerProviderStateMixin {
                           fontSize: 24)),
                 ),
                 Container(
+                    height: height / 10,
+                    width: width / 1.1,
+                    margin: EdgeInsets.only(top: height / 30),
+                    padding: EdgeInsets.symmetric(
+                        horizontal: width / 40, vertical: height / 30),
+                    
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          Container(
+                            child: CustomPaint(
+                              painter: ColorSelector(nivel),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+                Container(
                   alignment: Alignment.centerLeft,
                   margin: EdgeInsets.all(30),
                   child: Text(
@@ -81,5 +101,76 @@ class _ContagiosState extends State<Contagios> with TickerProviderStateMixin {
             ),
           ),
         ));
+  }
+}
+
+class ColorSelector extends CustomPainter {
+  String nivel;
+
+  ColorSelector(this.nivel);
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 4.0
+      ..color = nivel == 'Bajo'? Colors.green:Colors.grey;
+    final paint2 = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 4.0
+      ..color = nivel == 'Medio'? Colors.orange:Colors.grey;
+    final paint3 = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 4.0
+      ..color = nivel == 'Alto'? Colors.red:Colors.grey;
+
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(
+          Rect.fromLTWH(20, -15, 80, 35), Radius.circular(50)),
+      paint,
+    );
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(
+          Rect.fromLTWH(140, -15, 80, 35), Radius.circular(50)),
+      paint2,
+    );
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(
+          Rect.fromLTWH(260, -15, 80, 35), Radius.circular(50)),
+      paint3,
+    );
+    canvas.drawLine(Offset(100, 0), Offset(120, 0), paint);
+    canvas.drawLine(Offset(120, 0), Offset(140, 0), paint2);
+    canvas.drawLine(Offset(220, 0), Offset(240, 0), paint2);
+    canvas.drawLine(Offset(240, 0), Offset(260, 0), paint3);
+    TextSpan span =
+        new TextSpan(style: new TextStyle(color: Colors.white), text: 'Bajo');
+    TextPainter tp = new TextPainter(
+        text: span,
+        textAlign: TextAlign.left,
+        textDirection: TextDirection.ltr);
+    tp.layout();
+    tp.paint(canvas, new Offset(45, -5));
+    TextSpan span2 =
+        new TextSpan(style: new TextStyle(color: Colors.white), text: 'Medio');
+    TextPainter tp2 = new TextPainter(
+        text: span2,
+        textAlign: TextAlign.left,
+        textDirection: TextDirection.ltr);
+    tp2.layout();
+    tp2.paint(canvas, new Offset(160, -5));
+    TextSpan span3 =
+        new TextSpan(style: new TextStyle(color: Colors.white), text: 'Alto');
+    TextPainter tp3 = new TextPainter(
+        text: span3,
+        textAlign: TextAlign.left,
+        textDirection: TextDirection.ltr);
+    tp3.layout();
+    tp3.paint(canvas, new Offset(287, -5));
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
   }
 }
