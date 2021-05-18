c65f2221ab9838e9c8b2c5a7930f4a51b781934c
EyeWithItHandy <64708166+EyeWithItHandy@users.noreply.github.com>
2021-05-17 19:32:05 -0500

    visual inicio

--- lib/src/view/inicio.dart
+++ lib/src/view/inicio.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:aprendiendo/src/functions/generatePolygons.dart';
+import 'package:aprendiendo/src/view/Contagios.dart';
 import 'package:aprendiendo/src/view/qrScan.dart';
 import 'package:aprendiendo/src/widget/BottomPermisos.dart';
 import 'package:aprendiendo/src/widget/navbar.dart';
@@ -23,6 +24,7 @@ class Inicio extends StatefulWidget {
 }
 
 class _InicioState extends State<Inicio> with TickerProviderStateMixin {
+  String nivel = 'Bajo';
   bool _sesion = false;
   TabController _tabController;
   List<TaggedPolyline> _polygons = List();
@@ -71,6 +73,22 @@ class _InicioState extends State<Inicio> with TickerProviderStateMixin {
                         fontFamily: 'Roboto-Bold',
                         fontSize: 24)),
               ),
+              Container(
+                    height: height / 10,
+                    width: width / 1.1,
+                    margin: EdgeInsets.only(top: height / 30),
+                    
+                    child: Center(
+                      child: Row(
+                        children: [
+                          Container(
+                            child: CustomPaint(
+                              painter: ColorSelector(nivel),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
               
               Container(
                 alignment: Alignment.centerLeft,
@@ -127,6 +145,75 @@ class _InicioState extends State<Inicio> with TickerProviderStateMixin {
       });
     });
   }
+}
 
- 
+class ColorSelector extends CustomPainter {
+  String nivel;
+
+  ColorSelector(this.nivel);
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 4.0
+      ..color = nivel == 'Bajo' ? HexColor('#85C40C') : Colors.grey;
+    final paint2 = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 4.0
+      ..color = nivel == 'Medio' ? HexColor('#FBB03B') : Colors.grey;
+    final paint3 = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 4.0
+      ..color = nivel == 'Alto' ? HexColor('#ED1C24') : Colors.grey;
+
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(
+          Rect.fromLTWH(0, -15, 80, 35), Radius.circular(50)),
+      paint,
+    );
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(
+          Rect.fromLTWH(120, -15, 80, 35), Radius.circular(50)),
+      paint2,
+    );
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(
+          Rect.fromLTWH(240, -15, 80, 35), Radius.circular(50)),
+      paint3,
+    );
+    canvas.drawLine(Offset(80, 0), Offset(100, 0), paint);
+    canvas.drawLine(Offset(100, 0), Offset(120, 0), paint2);
+    canvas.drawLine(Offset(200, 0), Offset(220, 0), paint2);
+    canvas.drawLine(Offset(220, 0), Offset(240, 0), paint3);
+    TextSpan span =
+        new TextSpan(style: new TextStyle(color: Colors.white), text: 'Bajo');
+    TextPainter tp = new TextPainter(
+        text: span,
+        textAlign: TextAlign.left,
+        textDirection: TextDirection.ltr);
+    tp.layout();
+    tp.paint(canvas, new Offset(25, -5));
+    TextSpan span2 =->play 
+        new TextSpan(style: new TextStyle(color: Colors.white), text: 'Medio');
+    TextPainter tp2 = new TextPainter(
+        text: span2,
+        textAlign: TextAlign.left,
+        textDirection: TextDirection.ltr);
+    tp2.layout();
+    tp2.paint(canvas, new Offset(140, -5));
+    TextSpan span3 =
+        new TextSpan(style: new TextStyle(color: Colors.white), text: 'Alto');
+    TextPainter tp3 = new TextPainter(
+        text: span3,
+        textAlign: TextAlign.left,
+        textDirection: TextDirection.ltr);
+    tp3.layout();
+    tp3.paint(canvas, new Offset(267, -5));
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
 }
